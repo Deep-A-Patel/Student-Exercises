108fdd7af485063850248e45466835472f14eef0
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-01 08:52:37 -0700

    student exercise 3

--- Instructor.cs
+++ Instructor.cs
@@ -1,14 +1,10 @@
+    
 using System;
 
 namespace StudentExercises
 {
-    public class Instructor
+    public class Instructor : NSSPerson
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string SlackHandle { get; set; }
-        public int Cohort { get; set; }
-
         public void AssignStudents(Student student, Exercise exercise)
         {
             student.Exercises.Add(exercise);
